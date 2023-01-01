c79830ce591e551b4f337ff4972323121bb0ccd4
cizneeh <shin.echizen@gmail.com>
2023-01-02 00:08:14 +0900

    initial commit

---
+++ notes/hoge.md
@@ -0,0 +1,25 @@
+---
+tags:
+  - JavaScript
+  - Notion
+---
+
+# HOGE
+
+[何かリンク](https://zenn.dev/cizneeh)
+
+## test
+
+hoge
+
+- ho
+- ge
+
+```javascript
+function doSomething(){
+  // do something
+}
+
+doSomething()
+```
+ 
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { Client } from '@notionhq/client'
+import { getAllNotes } from './markdown.js'
+
+const token = process.env.NOTION_TOKEN
+const databaseId = process.env.NOTION_DATABASE_ID
+
+async function main() {
+  const notion = new Client({ auth: token })
+
+  const notes = getAllNotes('notes')
+  const failedNotes = []
+
+  for (const note of notes) {
+    try {
+      await notion.pages.create({
+        parent: { database_id: databaseId },
+        properties: {
+          // プロパティ名はcase sensitiveなので注意
+          Name: {
+            type: 'title',
+            title: [{ text: { content: note.name } }],
+          },
+          Tags: {
+            type: 'multi_select',
+            multi_select: note.tags.map(tag => ({ name: tag })),
+          },
+        },
+        children: note.body,
+      })
+    } catch (e) {
+      console.error(`${note.name}の追加に失敗: `, e)
+      failedNotes.push(note.name)
+    }
+  }
+
+  console.log('ページ作成に失敗したノート: ', failedNotes)
+}
+
+main()
